aad8fe1339f4035d55d47bd88b4461871054b567
turalcavadn <turaltj@code.edu.az>
2022-07-07 17:21:44 +0400

    added user model

---
+++ models/User.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+	{
+		username: {
+			type: String,
+			required: true,
+			min: 3,
+			max: 20,
+			unique: true,
+		},
+		email: {
+			type: String,
+			required: true,
+			max: 20,
+			unique: true,
+		},
+		password: {
+			type: String,
+			required: true,
+			min: 6,
+		},
+		profilePicture: {
+			type: String,
+			default: "",
+		},
+		followers: {
+			type: Array,
+			default: [],
+		},
+		following: {
+			type: Array,
+			default: [],
+		},
+		biography: {
+			type: String,
+			default: "",
+			max: 50,
+		},
+	},
+	{ timestamps: true }
+);
+
+module.exports = mongoose.model("User", UserSchema);
